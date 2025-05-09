91955b5fe0e156dfc8fc14db6a007e1fd968b14d
Marcela <marcela.sdeo4@gmail.com>
2025-05-09 17:06:40 -0300

    add character search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
+import { useState, useMemo } from 'react';
+
 export default function Home({ characters }) {
-  console.log('Personagens:', characters);
+  const [search, setSearch] = useState('');
+
+  const filteredCharacters = useMemo(() => {
+    return characters.filter((char) =>
+      char.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [characters, search]);
+
   return (
     <>
       <h1>Personagens de Star Wars</h1>
+      <input
+        type="text"
+        placeholder="Pesquise um personagem"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <div>
         <ul>
-          {characters.map((char) => (
+          {filteredCharacters.map((char) => (
             <li key={char.name}>{char.name}</li>
           ))}
         </ul>
