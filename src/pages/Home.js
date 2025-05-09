53ed4370832b1a496d333cfb8a5f1e9946befe09
Marcela <marcela.sdeo4@gmail.com>
2025-05-09 17:14:40 -0300

    add sorting by name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import { useState, useMemo } from 'react';
 
 export default function Home({ characters }) {
+
   const [search, setSearch] = useState('');
+  const [sortOrder, setSortOrder] = useState('ascending');
 
   const filteredCharacters = useMemo(() => {
     return characters.filter((char) =>
       char.name.toLowerCase().includes(search.toLowerCase())
     );
   }, [characters, search]);
+
+  const sortedCharacters = useMemo(() => {
+    return [...filteredCharacters].sort((firstCharacter, secondCharacter) => {
+      if (sortOrder === 'ascending') return firstCharacter.name.localeCompare(secondCharacter.name);
+      else return secondCharacter.name.localeCompare(firstCharacter.name);
+    });
+  }, [filteredCharacters, sortOrder]);
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevSortOrder) => (prevSortOrder === 'ascending' ? 'descending' : 'ascending'));
+  };
 
   return (
     <>
@@ -19,8 +32,11 @@ export default function Home({ characters }) {
         onChange={(e) => setSearch(e.target.value)}
       />
       <div>
+        <button onClick={toggleSortOrder}>
+          Ordenar {sortOrder === 'ascending' ? 'Z-A' : 'A-Z'}
+        </button>
         <ul>
-          {filteredCharacters.map((char) => (
+          {sortedCharacters.map((char) => (
             <li key={char.name}>{char.name}</li>
           ))}
         </ul>
