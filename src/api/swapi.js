3f8e23a27525ac53d408ae95237ae1b33b7d8fae
Marcela <marcela.sdeo4@gmail.com>
2025-05-10 15:56:15 -0300

    fetching all characters

--- src/api/swapi.js
+++ src/api/swapi.js
@@ -1,11 +1,18 @@
 const BASE_URL = 'https://swapi.py4e.com/api';
 
 export async function fetchCharacters() {
+  const characters = [];
+  let pages = `${BASE_URL}/people/?page=1`;
+
   try {
-    const response = await fetch(`${BASE_URL}/people`);
-    if (!response.ok) throw new Error('Erro ao buscar personagens.');
-    const data = await response.json();
-    return data.results;
+    while (pages) {
+      const response = await fetch(pages);
+      if (!response.ok) throw new Error('Erro ao buscar personagens.');
+      const data = await response.json();
+      characters.push(...data.results);
+      pages = data.next;
+    }
+    return characters;
   } catch (error) {
     console.error('Erro na API:', error);
     return [];
